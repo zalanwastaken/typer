d32803f5a2250019d04084db3160680427f998ed
zalanderwastaken <muditmishra.mr@gmail.com>
2024-04-11 11:25:05 +0530

    Added support for html-template

--- helpers/verify.lua
+++ helpers/verify.lua
@@ -15,6 +15,34 @@ function error(errordef)
         -- do nothing
     end
 end
+if not(love.filesystem.getInfo("html")) then
+    local template = "html-template"
+    local html_template = love.filesystem.getDirectoryItems(template)
+    local root = "html"
+    love.filesystem.createDirectory(root)
+    for i = 1, #html_template, 1 do
+        if love.filesystem.isDirectory(template.."/"..html_template[i]) then
+            love.filesystem.createDirectory(root.."/"..html_template[i])
+            local tmp = love.filesystem.getDirectoryItems(template.."/"..html_template[i])
+            for f = 1, #tmp, 1 do
+                html_template[#html_template + 1] = html_template[i].."/"..tmp[f]
+            end
+            table.remove(html_template, i)
+        end
+    end
+    for i = 1, #html_template, 1 do
+        local tmp = love.filesystem.read(template.."/"..html_template[i])
+        print(html_template[i])
+        if tmp ~= nil then
+            love.filesystem.write(root.."/"..html_template[i], tmp)
+        else
+            tmp = love.filesystem.read(html_template[i])
+            --print(tmp)
+            local success, two = love.filesystem.write(root.."/"..html_template[i], tmp)
+            print(success, two)
+        end
+    end
+end
 local files = { -- list/array of files 
     -- /dir
     "main.lua",
@@ -46,7 +74,8 @@ local files = { -- list/array of files
 local fnf = {} -- list of files that are not found 
 for i = 1, #files, 1 do -- verify the files here
     print("Verifing..."..files[i])
-    if not(love.filesystem.getInfo(files[i])) then
+    local tmp = love.filesystem.read(files[i])
+    if not(love.filesystem.getInfo(files[i])) and not(tmp ~= nil) then
         print(files[i].." Not found")
         fnf[#fnf + 1] = love.filesystem.getSource().."/"..files[i] -- add the files that are not found to fnf
     else
@@ -55,13 +84,13 @@ for i = 1, #files, 1 do -- verify the files here
 end
 if love.filesystem.getInfo("verification_log.log") == nil then
     love.filesystem.newFile("verification_log.log")
-    love.filesystem.write("verification_log.log", "First Boot\n"..os.time().."\n")
+    love.filesystem.write("verification_log.log", "First Boot\n"..os.date().."\n")
     local msg = love.window.showMessageBox("First boot", "This is the first boot \nWould you like to read the manuel ?", {"Yes", "No"}, "info")
     if msg == 1 then
-        love.system.openURL(love.filesystem.getSource().."/html/help.html")
+        love.system.openURL(__HTML__.."/help.html")
     end
 else
-    love.filesystem.write("verification_log.log", os.time()) -- clean the log
+    love.filesystem.write("verification_log.log", os.date()) -- clean the log
 end
 love.filesystem.write("verification_log.log", love.filesystem.read("verification_log.log").."\n--------------------")
 if #fnf > 0 then
@@ -80,10 +109,11 @@ if love.filesystem.isFused() then
 else
     love.filesystem.write("verification_log.log", love.filesystem.read("verification_log.log").."\nInstalltion is not fused: "..love.filesystem.getSource()) -- write fused status to log
 end
-love.filesystem.write("verification_log.log", love.filesystem.read("verification_log.log").."\n--------------------")
-love.filesystem.write("verification_log.log", love.filesystem.read("verification_log.log").."\nOS: "..love.system.getOS())
+love.filesystem.write("verification_log.log", love.filesystem.read("verification_log.log").."\n--------------------\nOS:"..love.system.getOS())
 print("Verification complete...")
 --print text art logo to console (Idk why but its cool)
 local textlogo = love.filesystem.read("data/logo.txt")
 print(textlogo)
 textlogo = nil
+files = nil
+fnf = nil 
