e178eb1a99578e073afc57d6c38abdc02476e96d
zalanwastaken <muditmishra.mr@gmail.com>
2024-07-06 18:49:34 +0530

    snapshort

--- helpers/commons.lua
+++ helpers/commons.lua
@@ -1,10 +1,12 @@
---common vars
+--! This file is runned at startup in the conf.lua file to setup vars that can be accesed from anywhere in the main thread
+--*common vars
 tmp = 0 --* init tmp (tmp is a general use variable)
 ar = {} --* user input array (ar = array)
 mode = "run" --* current mode of the program
+testedos = {"Windows", "Linux"} -- ? Tested OSes
 --* config vars
 __VER__ = [[2.0.1 "Munch"]] --! Version of the program
 __TYPE__ = "DEV" --! Type of the build
 __LOVEVER__ = 11.5 --! version of LOVE2D
 __HTML__ = love.filesystem.getAppdataDirectory().."love/typer/html" --* HTML dir
-forceerr = true --? force a error screen in dev mode
+forceerr = false --? force a error screen in dev mode
---
+++ helpers/verify/filelist.lua
@@ -0,0 +1,26 @@
+local files = { -- list/array of files 
+    -- /dir
+    "main.lua",
+    "conf.lua",
+    --"licence.txt",
+    -- /libs dir
+    "libs/sav.lua",
+    "libs/funcs.lua",
+    "libs/jsonlib.lua",
+    -- /data dir
+    "data/icon.png",
+    "data/name.png",
+    "data/settings.json",
+    "data/logo.txt",
+    "data/sounds/ping.mp3",
+    -- /html dir
+    "html/index.html",
+    "html/images/love.ico",
+    "html/data/love2d-license.txt",
+    "html/info.html",
+    "html/images/icon.png",
+    "html/help.html",
+    -- /helpers dir
+    "helpers/commons.lua"
+}
+return(files)
