f9ecd6a151eaa9436f4c68db4b717f652502c469
zalanwastaken <muditmishra.mr@gmail.com>
2024-08-02 19:39:34 +0530

    fixed comment formatting

--- conf.lua
+++ conf.lua
@@ -7,14 +7,14 @@ else
 end
 -- * config func
 function love.conf(t)
-    t.version = __LOVEVER__ -- * set the love2d version
+    t.version = __LOVEVER__ --* set the love2d version
     if __TYPE__== "DEV" then
         t.window.title = "DEV BUILD "..__VER__
-        t.console = true -- * set the console
+        t.console = true --* set the console
     else
         t.window.title = "Typer "..__VER__ -- * set the window title
-        t.console = false -- * set the console
+        t.console = false --* set the console
     end
-    t.window.resizable = true -- * make the window resizeable
-    love.setDeprecationOutput(false) -- ! Probably not a good idea
+    t.window.resizable = true --* make the window resizeable
+    love.setDeprecationOutput(false) --! Probably not a good idea
 end
